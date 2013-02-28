dd60f0641378cc3eb130c4ccee6cc3c8d33ab502
dekokun <shintaro.kurachi@gmail.com>
2013-02-28 18:31:42 +0900

    demoTest add

---
+++ autoTest/demoTest.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: pushTestCommon.php 823 2012-12-28 02:58:26Z shintaro_kurachi $
+
+/**
+ * Description of pushTestCommon
+ *
+ * @author n00823
+ */
+class DemoTest extends PHPUnit_Framework_TestCase{
+  public function test_demo1() {
+    $this->assertEquals(1, 1);
+  }
+}
